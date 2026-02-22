7211de024299bd9f348c8fe9547f3b2551caaef6
urbanoliver <urbanoliver777@gmail.com>
2026-02-22 11:35:49 +0100

    switch to vs 2026

--- smart-todo/backend/SmartTodo.Api/obj/Debug/net8.0/SmartTodo.Api.AssemblyInfo.cs
+++ smart-todo/backend/SmartTodo.Api/obj/Debug/net8.0/SmartTodo.Api.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SmartTodo.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4477a29e60f88e0a7f81f7a7751d99173d289980")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8ec4200acc5e38659c9cb3f922af2ed7e5ab5afd")]
 [assembly: System.Reflection.AssemblyProductAttribute("SmartTodo.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SmartTodo.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
